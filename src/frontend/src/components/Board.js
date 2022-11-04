d98c546521283be2c8e4715965d6dd36454ad79b
LupoX1 <kernel_panic_x@protonmail.com>
2022-11-04 10:27:23 +0100

    fix board

--- src/frontend/src/components/Board.js
+++ src/frontend/src/components/Board.js
@@ -1,101 +1,119 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React from 'react'
 import axios from 'axios'
 
-const Board = ({ gameId }) => {
+const smileCool = <>&#128526;</>
+const smileHappy = <>&#128578;</>
+const smileWorry = <>&#128558;</>
+const smileSad = <>&#128577;</>
+const question = <>&#10067;</>
+const flag = <>&#128681;</>
+const bomb = <>&#128163;</>
+const skull = <>&#128128;</>
+const dice = <>&#127922;</>
 
-    const smileCool = <>&#128526;</>
-    const smileHappy = <>&#128578;</>
-    const smileWorry = <>&#128558;</>
-    const smileSad = <>&#128577;</>
-    const question = <>&#10067;</>
-    const flag = <>&#128681;</>
-    const bomb = <>&#128163;</>
-    const skull = <>&#128128;</>
-    const dice = <>&#127922;</>
+const colors = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
 
-    const colors = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
+class Board extends React.Component {
 
-    const [gameData, setGameData] = useState(null)
-    const [smile, setSmile] = useState(smileHappy)
-    const [cells, setCells] = useState([])
-    const [time, setTime] = useState(0)
-    const [loaded, setLoaded] = useState(false)
+    constructor(props) {
+        super(props)
 
-    const [currentCell, setCurrentCell] = useState(null)
+        this.state = {
+            gameData: {},
+            currentCell: {},
+            cells: [],
+            time: 0,
+            loaded: false,
+            smile: smileHappy,
+            timer : null
+        }
 
-    const stateRef = useRef(cells)
+        this.handleClickDown = this.handleClickDown.bind(this)
+        this.handleClickUp = this.handleClickUp.bind(this)
+        this.noAction = this.noAction.bind(this)
+        this.startGame = this.startGame.bind(this)
+        this.discoverCell = this.discoverCell.bind(this)
+        this.flagCell = this.flagCell.bind(this)
+        this.disableMenu = this.disableMenu.bind(this)
+        this.countTime = this.countTime.bind(this)
+    }
 
-    const handleClickDown = (e) => {
+    handleClickDown = (e) => {
         e.preventDefault()
 
-        if (e.target.nodeName.toLowerCase() !== 'td' || gameData.won || gameData.lost) {
+        if (e.target.nodeName.toLowerCase() !== 'td' || this.state.gameData.won || this.state.gameData.lost) {
             return
         }
 
         let row = parseInt(e.target.dataset.row)
         let column = parseInt(e.target.dataset.column)
 
-        let currentCell = cells[row][column]
+        let currentCell = {...this.state.cells[row][column]}
         if (currentCell.visited) {
             return
         }
 
-        let cells = cells.map(cell => {
-            if (cell.row === row && cell.column === column) {
-                return { ...cell, state: 'clicked' }
-            }
-            return cell
-        })
+        let cells = this.state.cells.map(row => [...row])
+        cells[row][column].state = 'clicked'
 
-        setCurrentCell(currentCell)
-        setCells(cells)
-        setSmile(smileWorry)
+        this.setState({
+            currentCell: currentCell,
+            cells: cells,
+            smile: smileWorry
+        })
     }
 
-    const handleClickUp = (e) => {
+    handleClickUp = (e) => {
         e.preventDefault()
 
-        if (e.target.nodeName.toLowerCase() !== 'td' || gameData.won || gameData.lost || currentCell.visited) {
+        if (e.target.nodeName.toLowerCase() !== 'td' || this.state.gameData.won || this.state.gameData.lost || this.state.currentCell.visited) {
             return
         }
 
-        let startHandler = gameData.startDate ? noAction : startGame
-        let clickHandler = e.button === 2 ? flagCell : discoverCell
+        let startHandler = this.state.gameData.startDate ? this.noAction : this.startGame
+        let clickHandler = e.button === 2 ? this.flagCell : this.discoverCell
 
         let row = parseInt(e.target.dataset.row)
         let column = parseInt(e.target.dataset.column)
 
-        console.log(currentCell)
+        console.log(this.state.currentCell)
 
         startHandler(row, column).then(clickHandler)
     }
 
-    const noAction = async (row, column) => {
+    noAction = (row, column) => {
         return Promise.resolve({ row, column })
     }
 
-    const startGame = async (row, column) => {
-        return axios.put('/game/' + gameData.gameId)
+    startGame = (row, column) => {
+        return axios.put('/game/' + this.props.gameId)
             .then(response => {
-                setGameData({
-                    ...gameData,
-                    won: response.data.won,
-                    lost: response.data.lost,
-                    discoveredCells: response.data.discoveredCells,
-                    flaggedMines: response.data.flaggedMines,
-                    startDate: response.data.startDate
+                let timer = setInterval(this.countTime, 1000)
+                this.setState({
+                    gameData : {
+                        ...this.state.gameData,
+                        startDate: response.data.startDate,
+                    },
+                    timer : timer
                 })
-                setTime(0)
                 return { row, column }
             })
     }
 
-    const discoverCell = async (response) => {
-        axios.put('/board/' + gameData.boardId + '/cell/' + response.row + '/' + response.column + '/discover')
+    countTime = () => {
+        this.setState({
+            time : this.state.time + 1
+        })
+    }
+
+    discoverCell = (response) => {
+        let row = response.row
+        let column = response.column
+        axios.put('/board/' + this.state.gameData.boardId + '/cell/' + row + '/' + column + '/discover')
             .then(response => {
-                let updatedCells = []
+                let cells = this.state.cells.map(row => [...row])
                 response.data.cells.forEach(c => {
-                    let currentCell = { ...cells[c.row][c.column] }
+                    let currentCell = cells[c.row][c.column]
                     if (c.bomb) {
                         currentCell.content = bomb
                     }
@@ -108,77 +126,72 @@ const Board = ({ gameId }) => {
                     }
 
                     currentCell.visited = true
-                    currentCell.state = 'visited ' + colors[c.nearBombs - 1]
-                    updatedCells.push(currentCell)
+                    currentCell.state = 'visited ' + colors[c.nearBombs]
                 })
 
-                let cells = this.state.cells.map(oldCell => {
-                    const newCell = updatedCells.find(c => c.row === oldCell.row && c.column === oldCell.column)
-                    return newCell ? { ...oldCell, ...newCell } : oldCell
-                })
-
+                let timer = this.state.timer
                 let smile = smileHappy
-                let lost = false
-                let won = false
                 if (response.data.lose) {
                     smile = skull
-                    lost = true
+                    clearInterval(timer)
+                    timer = null
                 } else if (response.data.won) {
                     smile = smileCool
-                    won = true
                 }
 
-                setGameData({
-                    ...gameData,
-                    won: won,
-                    lost: lost,
-                    discoveredCells: response.data.discoveredCells,
-                    totalMines: response.data.totalMines
-                })
-                setSmile(smile)
-                setCurrentCell(null)
-            })
-    }
-
-    const flagCell = async (response) => {
-        axios.put('/board/' + this.state.boardId + '/cell/' + response.row + '/' + response.column + '/flag')
-            .then(response => {
-                let row = response.data.row
-                let column = response.data.column
-                let cells = this.state.cells.map(oldCell => {
-                    if (oldCell.row === row && oldCell.column === column) {
-                        let cell = { ...oldCell }
-                        if (response.data.flagged) {
-                            cell.content = flag
-                        } else if (response.data.marked) {
-                            cell.content = question
-                        } else {
-                            cell.content = ''
-                        }
-                        cell.state = 'normal'
-                        return cell
-                    }
-                    return oldCell
-                })
-
-                setGameData({
+                this.setState({
+                    smile: smile,
+                    currentCell: {},
                     cells: cells,
-                    discoveredCells: response.data.discoveredCells,
-                    totalMines: response.data.totalMines
+                    timer: timer,
+                    gameData: {
+                        ...this.state.gameData,
+                        won: response.data.won,
+                        lost: response.data.lost,
+                        discoveredCells: response.data.cells.length
+                    }
                 })
-                setSmile(smileHappy)
-                setCurrentCell(null)
             })
     }
 
-    const disableMenu = async (e) => {
+    flagCell = (response) => {
+        let row = response.row
+        let column = response.column
+        axios.put('/board/' + this.state.gameData.boardId + '/cell/' + row + '/' + column + '/flag')
+            .then(response => {
+                let cells = this.state.cells.map(row => [...row])
+                let cell = cells[row][column]
+                      
+                if (response.data.flagged) {
+                    cell.content = flag
+                } else if (response.data.marked) {
+                    cell.content = question
+                } else {
+                    cell.content = ''
+                }
+                cell.state = 'normal'
+                      
+                this.setState({
+                    smile: smileHappy,
+                    currentCell: {},
+                    cells: cells,
+                    gameData: {
+                        ...this.state.gameData,
+                        discoveredCells: response.data.discoveredCells,
+                        totalMines: response.data.totalMines
+                    }
+                })
+            })
+    }
+
+    disableMenu = (e) => {
         e.preventDefault()
 
         return false
     }
 
-    useEffect(() => {
-        axios.get('game/' + gameId)
+    componentDidMount = () => {
+        axios.get('game/' + this.props.gameId)
             .then(response => {
                 let rows = response.data.cells
                 let cells = []
@@ -191,44 +204,54 @@ const Board = ({ gameId }) => {
                     cells.push(row)
                 })
 
-                console.log(cells)
-
-                setCells(cells)
-                setGameData(response.data)
-                setLoaded(true)
-            })
-    }, [])
-
-    if (!loaded) {
-        return <div>Loading...</div>
-    }
-
-    return (
-        <table className='board' onMouseDown={handleClickDown} onMouseUp={handleClickUp} onContextMenu={disableMenu}>
-            <thead>
-                <tr>
-                    <th colSpan='3'>{gameData.totalMines}</th>
-                    <th colSpan={gameData.columns - 6}>{smile}</th>
-                    <th colSpan='3'>{time}</th>
-                </tr>
-            </thead>
-            <tbody>
-                {cells.map((row, i) => {
-                    return (
-                        <tr key={i}>{
-                            row.map(cell => {
-                                return (
-                                    <td key={cell.row + '-' + cell.column} data-row={cell.row} data-column={cell.column} className={cell.state}>{cell.content}</td>
-                                )
-                            }
-                            )}
-                        </tr>
-                    )
+                let time = response.data.duration || 0
+                let startDate = response.data.startDate || null
+                let timer = null
+                if(startDate && time > 0){
+                    timer = setInterval(this.countTime, 1000)
                 }
-                )}
-            </tbody>
-        </table>
-    )
+
+                this.setState({
+                    cells: cells,
+                    gameData: response.data,
+                    time : time,
+                    loaded: true,
+                    timer : timer
+                })
+            })
+    }
+    render = () => {
+        if (!this.state.loaded) {
+            return <div>Loading...</div>
+        }
+
+        return (
+            <table className='board' onMouseDown={this.handleClickDown} onMouseUp={this.handleClickUp} onContextMenu={this.disableMenu} >
+                <thead>
+                    <tr>
+                        <th colSpan='3'>{this.state.gameData.totalMines}</th>
+                        <th colSpan={this.state.gameData.columns - 6}>{this.state.smile}</th>
+                        <th colSpan='3'>{this.state.time}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.cells.map((row, i) => {
+                        return (
+                            <tr key={i}>{
+                                row.map(cell => {
+                                    return (
+                                        <td key={cell.row + '-' + cell.column} data-row={cell.row} data-column={cell.column} className={cell.state}>{cell.content}</td>
+                                    )
+                                }
+                                )}
+                            </tr>
+                        )
+                    }
+                    )}
+                </tbody>
+            </table>
+        )
+    }
 
 }
 
